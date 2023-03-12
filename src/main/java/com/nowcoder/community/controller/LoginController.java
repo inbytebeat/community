f24538759b09157525c1a00786cc5fb5690bfb32
inbytebeat <1515577847@qq.com>
2023-03-13 01:49:25 +0800

    已实现用户激活功能 用户注册完成

--- src/main/java/com/nowcoder/community/controller/LoginController.java
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.nowcoder.community.controller;
 
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,7 +19,7 @@ import java.util.Map;
  */
 
 @Controller
-public class LoginController {
+public class LoginController implements CommunityConstant {
 
     @Autowired
     private UserService userService;
@@ -26,6 +27,11 @@ public class LoginController {
     @RequestMapping(path = "/register",method = RequestMethod.GET)
     public String getRegisterPage() {
         return "/site/register";
+    }
+
+    @RequestMapping(path = "/login",method = RequestMethod.GET)
+    public String getLoginPage() {
+        return "/site/login";
     }
 
     /**
@@ -55,9 +61,31 @@ public class LoginController {
     }
 
     //http://localhost:8080/community/activation/userId/激活码
-    @RequestMapping(path = "/activation/#{userId}/#{code}",method = RequestMethod.GET)
-    public String actication(Model model, @PathVariable("userId")int userId) {
-        return null;
+
+    /**
+     * 用于用户账号激活
+     * @param model
+     * @param userId
+     * @param code
+     * @return
+     */
+    @RequestMapping(path = "/activation/{userId}/{code}",method = RequestMethod.GET)
+    public String actication(Model model, @PathVariable("userId")int userId,@PathVariable("code")String code) {
+        int activation = userService.activation(userId, code);
+        if(activation  == ACTIVATION_SUCCESS){
+            model.addAttribute("msg","激活成功，您的账号已经可以正常使用了");
+            // 设置激活成功后的跳转路径
+            model.addAttribute("target","/login");
+        }else if (activation  == ACTIVATION_REPEAT) {
+            model.addAttribute("msg","请不要重复激活，该账号已经激活");
+            // 设置激活成功后的跳转路径
+            model.addAttribute("target","/index");
+        }else {
+            model.addAttribute("msg","激活失败，激活码有误");
+            // 设置激活成功后的跳转路径
+            model.addAttribute("target","/index");
+        }
+        return "site/operate-result";
     }
 
 
