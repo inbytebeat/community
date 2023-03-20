f5b62498fa1492f570f10f221f496cfe3180616b
inbytebeat <1515577847@qq.com>
2023-03-20 20:43:01 +0800

    部署前的备份

---
+++ src/main/java/com/nowcoder/community/CommunityServletInitializer.java
@@ -0,0 +1,17 @@
+package com.nowcoder.community;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @Author: XTY~
+ * @CreateTime: 20/3/2023 下午8:41
+ * @Description:
+ */
+
+public class CommunityServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CommunityApplication.class);
+    }
+}
