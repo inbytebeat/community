3b7a24764c0badcc1f8afbcc8ac75de4debd9868
inbytebeat <1515577847@qq.com>
2023-03-20 18:33:03 +0800

    使用缓存优化查询热帖功能完成

--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -1,17 +1,28 @@
 package com.nowcoder.community.service;
 
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.util.SensitiveFilter;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.HtmlUtils;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class DiscussPostService {
+
+    private static final Logger logger = LoggerFactory.getLogger(DiscussPostService.class);
 
     @Autowired
     private DiscussPostMapper discussPostMapper;
@@ -19,12 +30,89 @@ public class DiscussPostService {
     @Autowired
     private SensitiveFilter sensitiveFilter;
 
+    /**
+     * 使用本地缓存能存的数据最大数量
+     */
+    @Value("${caffeine.posts.max-size}")
+    public int maxSize;
+
+    /**
+     * 缓存的过期时间
+     */
+    @Value("${caffeine.posts.expire-seconds}")
+    public int expiredSeconds;
+
+    /**
+     * 帖子列表缓存
+     */
+    private LoadingCache<String, List<DiscussPost>> postListCache;
+
+    /**
+     * 帖子总数缓存
+     */
+    private LoadingCache<Integer, Integer> postRowCache;
+
+    @PostConstruct
+    public void init() {
+        // 初始化帖子列表缓存
+        postListCache = Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expiredSeconds, TimeUnit.SECONDS)
+                .build(new CacheLoader<String, List<DiscussPost>>() {
+                    @Nullable
+                    @Override
+                    // 当我们需要从caffeine中取出缓存数据时，caffeine会先在缓存中查询是否有数据，如果没有则执行下面的逻辑 到数据库中查询数据并存入缓存中，然后返回
+                    public List<DiscussPost> load(@NonNull String key) throws Exception {
+                        if (key == null || key.length() == 0) {
+                            throw new IllegalArgumentException("参数错误!");
+                        }
+
+                        String[] params = key.split(":");
+                        if (params == null || params.length != 2) {
+                            throw new IllegalArgumentException("参数错误!");
+                        }
+
+                        int offset = Integer.valueOf(params[0]);
+                        int limit = Integer.valueOf(params[1]);
+
+                        // 这里也可以再添加访问redis获取数据，最后再从db中查询数据 二级缓存: Redis -> mysql
+
+                        logger.debug("load post list from DB.");
+                        return discussPostMapper.selectDiscussPosts(0, offset, limit, 1);
+                    }
+                });
+        // 初始化帖子总数缓存
+        postRowCache = Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expiredSeconds, TimeUnit.SECONDS)
+                .build(new CacheLoader<Integer, Integer>() {
+                    @Nullable
+                    @Override
+                    public Integer load(@NonNull Integer key) throws Exception {
+                        logger.debug("load post rows from DB.");
+                        return discussPostMapper.selectDiscussPostRows(key);
+                    }
+                });
+    }
+
+
 
     public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit, int orderMode) {
+        // 只有访问热门贴子数据的时候才进行缓存
+        if(userId == 0 && orderMode == 1) {
+            // 直接从缓存中返回结果
+            return postListCache.get(offset + ":" + limit);
+        }
+        logger.debug("load post list from DB.");
         return discussPostMapper.selectDiscussPosts(userId, offset, limit, orderMode);
     }
 
     public int findDiscussPostRows(int userId) {
+        if(userId == 0) {
+            // 直接从缓存中返回结果
+            return postRowCache.get(userId);
+        }
+        logger.debug("load post rows from DB.");
         return discussPostMapper.selectDiscussPostRows(userId);
     }
 
